03d31d46994422f50954f7093442328582432c8f
ludovicaschaerf <ludovica.schaerf@gmail.com>
2020-04-03 17:03:20 +0200

    added test set and predictions

--- multi-label/CNN_RNN/train.py
+++ multi-label/CNN_RNN/train.py
@@ -4,6 +4,7 @@ import torch.nn as nn
 import numpy as np
 import os
 import pickle
+import json
 from torch.utils.data import DataLoader
 from build_vocab import Vocabulary
 from data_loader import get_loader
@@ -34,9 +35,31 @@ def main(args):
         vocab = pickle.load(f)
     print("build data loader ...")
     # Build data loader
-    data_loader = get_loader(args.image_dir, args.idx2path_path, args.caption_path, vocab, 
-                             args.batch_size, shuffle=True, transform=transform,
-                             num_workers=args.num_workers) 
+    with open(args.caption_path, 'r') as infile:
+        caption = json.load(infile)
+    with open(args.idx2path_path, 'r') as infile:
+        idx2path = json.load(infile)
+    caption_train = {}
+    caption_test = {}
+    idx2path_train = {}
+    idx2path_test = {}
+    for i,capt in enumerate(caption):
+        if i > 20000:
+            caption_test[capt] = caption[capt]
+            idx2path_test[str(i-20000)] = idx2path[str(i)]
+            continue
+        caption_train[capt] = caption[capt]
+        idx2path_train[str(i)] = idx2path[str(i)]
+    print(len(caption_test.keys()), len(caption_train.keys()), len(idx2path_test.keys()), len(idx2path_train.keys()))
+    
+    data_loader_train = get_loader(args.image_dir, idx2path_train, caption_train, vocab, 
+                                 args.batch_size, shuffle=True, transform=transform,
+                                 num_workers=args.num_workers) 
+   
+    
+    data_loader_test = get_loader(args.image_dir, idx2path_test, caption_test, vocab, 
+                                 args.batch_size, shuffle=True, transform=transform,
+                                 num_workers=args.num_workers) 
     
     print("build the models ...")
     # Build the models
@@ -52,9 +75,9 @@ def main(args):
     optimizer = torch.optim.Adam(params, lr=args.learning_rate)
     
     # Train the models
-    total_step = len(data_loader)
+    total_step = len(data_loader_train)
     for epoch in range(args.num_epochs):
-        for i, (images, captions, lengths) in enumerate(data_loader):
+        for i, (images, captions, lengths) in enumerate(data_loader_train):
             print(i)
             # Set mini-batch dataset
             images = images.to(device)
@@ -82,26 +105,38 @@ def main(args):
                     args.model_path, 'decoder-{}-{}.ckpt'.format(epoch+1, i+1)))
                 torch.save(encoder.state_dict(), os.path.join(
                     args.model_path, 'encoder-{}-{}.ckpt'.format(epoch+1, i+1)))
-
+    
+    for i, (images, captions, lengths) in enumerate(data_loader_test):
+            print(i)
+            # Set mini-batch dataset
+            images = images.to(device)
+            captions = captions.to(device)
+            targets = pack_padded_sequence(captions, lengths, batch_first=True)[0]
+            
+            # Forward, backward and optimize
+            features = encoder(images)
+            predictions = decoder.forward(features, captions, lengths)
+            with open('../predictions_CNN_RNN.pkl', 'wb') as outfile:
+                pickle.dump((images, predictions), outfile)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model_path', type=str, default='.' , help='path for saving trained models')
+    parser.add_argument('--model_path', type=str, default='./' , help='path for saving trained models')
     parser.add_argument('--crop_size', type=int, default=224 , help='size for randomly cropping images')
     parser.add_argument('--vocab_path', type=str, default='../../data/zh_vocab.pkl', help='path for vocabulary wrapper')
     parser.add_argument('--image_dir', type=str, default='../../../data_tate', help='directory for resized images') #data/resized2014
     parser.add_argument('--caption_path', type=str, default='../../data/MyData.json', help='path for train annotation json file')
     parser.add_argument('--idx2path_path', type=str, default='../../data/idx2path.json', help='path for train idx2path json file')
     parser.add_argument('--log_step', type=int , default=10, help='step size for prining log info')
-    parser.add_argument('--save_step', type=int , default=1000, help='step size for saving trained models')
+    parser.add_argument('--save_step', type=int , default=700, help='step size for saving trained models')
     
     # Model parameters
     parser.add_argument('--embed_size', type=int , default=512, help='dimension of word embedding vectors')
     parser.add_argument('--hidden_size', type=int , default=1024, help='dimension of lstm hidden states')
     parser.add_argument('--num_layers', type=int , default=1, help='number of layers in lstm')
     
-    parser.add_argument('--num_epochs', type=int, default=5)
-    parser.add_argument('--batch_size', type=int, default=128)
+    parser.add_argument('--num_epochs', type=int, default=3)
+    parser.add_argument('--batch_size', type=int, default=32)
     parser.add_argument('--num_workers', type=int, default=1)
     parser.add_argument('--learning_rate', type=float, default=0.001)
     args = parser.parse_args()
