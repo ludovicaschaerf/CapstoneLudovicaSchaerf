65d468a3b52012477872bf128ca5f0cf6443929b
ludovicaschaerf <ludovica.schaerf@gmail.com>
2020-04-13 10:51:02 +0200

    made option resume_training

--- multi-label/CNN_RNN/train.py
+++ multi-label/CNN_RNN/train.py
@@ -62,66 +62,67 @@ def main(args):
                                  num_workers=args.num_workers) 
     
     print("build the models ...")
+    
     # Build the models
     encoder = EncoderCNN(args.embed_size).to(device)
     decoder = DecoderRNN(args.embed_size, args.hidden_size, len(vocab), args.num_layers).to(device)
-    #encoder.load_state_dict(torch.load("models/encoder-2-1000.ckpt"))
-    #decoder.load_state_dict(torch.load("models/decoder-2-1000.ckpt")) 
-
-
-    # Loss and optimizer
-    criterion = nn.CrossEntropyLoss()
-    params = list(decoder.parameters())# + list(encoder.linear.parameters()) + list(encoder.bn.parameters())
-    optimizer = torch.optim.Adam(params, lr=args.learning_rate)
     
-    # Train the models
-    total_step = len(data_loader_train)
-    for epoch in range(args.num_epochs):
-        for i, (images, captions, lengths, path) in enumerate(data_loader_train):
-            print(i)
-            # Set mini-batch dataset
-            images = images.to(device)
-            captions = captions.to(device)
-            targets = pack_padded_sequence(captions, lengths, batch_first=True)[0]
-            
-            # Forward, backward and optimize
-            features = encoder(images)
-            outputs = decoder(features, captions, lengths)
-            outputs = pack_padded_sequence(outputs, lengths, batch_first=True)[0]
-            loss = criterion(outputs, targets)
-            decoder.zero_grad()
-            encoder.zero_grad()
-            loss.backward()
-            optimizer.step()
-
-            # Print log info
-            if i % args.log_step == 0:
-                print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}, Perplexity: {:5.4f}'
-                      .format(epoch, args.num_epochs, i, total_step, loss.item(), np.exp(loss.item()))) 
+    if not args.resume_training:
+        # Loss and optimizer
+        criterion = nn.CrossEntropyLoss()
+        params = list(decoder.parameters())# + list(encoder.linear.parameters()) + list(encoder.bn.parameters())
+        optimizer = torch.optim.Adam(params, lr=args.learning_rate)
+        
+        # Train the models
+        total_step = len(data_loader_train)
+        for epoch in range(args.num_epochs):
+            for i, (images, captions, lengths, path) in enumerate(data_loader_train):
+                # Set mini-batch dataset
+                images = images.to(device)
+                captions = captions.to(device)
+                targets = pack_padded_sequence(captions, lengths, batch_first=True)[0]
                 
-            # Save the model checkpoints
-            if (i+1) % args.save_step == 0:
-                torch.save(decoder.state_dict(), os.path.join(
-                    args.model_path, 'decoder-{}-{}.ckpt'.format(epoch+1, i+1)))
-                torch.save(encoder.state_dict(), os.path.join(
-                    args.model_path, 'encoder-{}-{}.ckpt'.format(epoch+1, i+1)))
-    
-    dict_test = {}
-    for i, (images, captions, lengths, path) in enumerate(data_loader_test):
-            print(i)
-            # Set mini-batch dataset
-            images = images.to(device)
-            captions = captions.to(device)
-            targets = pack_padded_sequence(captions, lengths, batch_first=True)[0]
-            
-            # Forward, backward and optimize
-            features = encoder(images)
-            outputs = decoder(features, captions, lengths)
-            outputs = pack_padded_sequence(outputs, lengths, batch_first=True)[0]
-            dict_test[path] = (outputs, target)
-    
-    with open('../results/predictions_CNN_RNN.pkl', 'wb') as outfile:
-        pickle.dump(dict_test, outfile)
+                # Forward, backward and optimize
+                features = encoder(images)
+                outputs = decoder(features, captions, lengths)
+                outputs = pack_padded_sequence(outputs, lengths, batch_first=True)[0]
+                loss = criterion(outputs, targets)
+                decoder.zero_grad()
+                encoder.zero_grad()
+                loss.backward()
+                optimizer.step()
+        
+                # Print log info
+                if i % args.log_step == 0:
+                    print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}, Perplexity: {:5.4f}'
+                          .format(epoch, args.num_epochs, i, total_step, loss.item(), np.exp(loss.item()))) 
+                    
+                # Save the model checkpoints
+                if (i+1) % args.save_step == 0:
+                    torch.save(decoder.state_dict(), os.path.join(
+                        args.model_path, 'decoder-{}-{}.ckpt'.format(epoch+1, i+1)))
+                    torch.save(encoder.state_dict(), os.path.join(
+                        args.model_path, 'encoder-{}-{}.ckpt'.format(epoch+1, i+1)))
+    else:
+        encoder.load_state_dict(torch.load("models/encoder-30-100.ckpt"))
+        decoder.load_state_dict(torch.load("models/decoder-30-100.ckpt")) 
+
+        dict_test = {}
+        for i, (images, captions, lengths, path) in enumerate(data_loader_test):
+                print(i)
+                # Set mini-batch dataset
+                images = images.to(device)
+                captions = captions.to(device)
+                targets = pack_padded_sequence(captions, lengths, batch_first=True)[0]
+                
+                # Forward, backward and optimize
+                features = encoder(images)
+                outputs = decoder(features, captions, lengths)
+                outputs = pack_padded_sequence(outputs, lengths, batch_first=True)[0]
+                dict_test[path] = (outputs, targets)
+        
+        with open('../results/predictions_CNN_RNN.pkl', 'wb') as outfile:
+            pickle.dump(dict_test, outfile)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -133,6 +134,7 @@ if __name__ == '__main__':
     parser.add_argument('--idx2path_path', type=str, default='../../data/results/idx2path.json', help='path for train idx2path json file')
     parser.add_argument('--log_step', type=int , default=10, help='step size for prining log info')
     parser.add_argument('--save_step', type=int , default=700, help='step size for saving trained models')
+    parser.add_argument('--resume_training' , default=False, help='to resume training')
     
     # Model parameters
     parser.add_argument('--embed_size', type=int , default=512, help='dimension of word embedding vectors')
